83bb7096ef0370adb55adcce9af6b21f0eb13b93
tsainei yuan <hi@tsainei.com>
2022-05-17 14:33:44 +0200

    finish challenge

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[show update edit destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  def edit; end
+
+  def show; end
+
+  def update
+    @task.update(task_params)
+
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  def completed?
+    @task.completed
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
